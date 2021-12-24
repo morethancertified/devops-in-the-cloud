ca4dc53204ac9e6ad3054b47ba264677aabfe2f6
Derek Morgan <derekm1215@gmail.com>
2021-12-24 18:10:44 +0000

    added lessons

---
+++ 04-Jenkins/14-Configure_Multiple_Branches/providers.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-1"
+  shared_credentials_file="/home/ubuntu/.aws/credentials"
+}
